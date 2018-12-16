8242d169b7e86619541263b1bfdffa8046108061
Zuzanna <39634526+xkadmx@users.noreply.github.com>
2018-12-16 10:42:13 +0100

    Dodałam 3 klasy

---
+++ Projekt Z/src/pies.java
@@ -0,0 +1,10 @@
+public class pies {
+    public void idzPan() {
+        System.out.println("Do domu!");
+    }
+
+    public void greeting(String name) {
+        System.out.println("No miło Cię tu widzieć, " + name + "u :)");
+    }
+}
+
